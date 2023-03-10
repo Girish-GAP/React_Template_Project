098acb3a73d58e99cd3fc4ca25a4c9889efd5edd
Girish <girishpawar055@gmail.com>
2023-03-10 14:59:33 +0530

    added login feature

---
+++ notes.js
@@ -0,0 +1,53 @@
+// npx create-react-app --template typescript
+
+// install the sass : npm i sass
+// add assets from tickit
+
+
+// install @mui/material
+// npm install @mui/material @emotion/react @emotion/styled
+// create cusotme theme by mui createTheme function
+
+// added <StyledEngineProvider, injectFirst><CssBaseline /> , theme in index.html and wrap around App component, its for styling
+// for navigation : npm i react-router-dom 
+
+// for redux : 
+ // npm install @reduxjs/toolkit 
+ // npm i react-redux
+
+// add redux file structure in utils
+
+
+// created storage folder in utils and added session storage file init as index.js
+
+// Created authentication slice
+//  - add intial values and reducers
+//  - export the reducers
+//  - export the values of reducers
+// add store in index.tsx
+
+// add routes in src
+
+// for material ui
+// npm install @mui/material @emotion/react @emotion/styled
+//     "@mui/icons-material": "^5.11.0",
+
+
+// added Form Component : 
+//  Added all components which form components needs
+
+
+// use npm update before adding packages // it will update the core pacakges...
+
+
+// "@mui/x-date-pickers": "^5.0.6",
+
+// npm i react-localization
+
+// npm i react-ckeditor-component
+
+
+// npm i --save react-select  "^5.7.0", /** Deprectiated */
+// npm install --save @types/react-select   : npm WARN deprecated @types/react-select@5.0.1:
+
+// "react-material-file-upload": "^0.0.4",      /** Deprectiated */
